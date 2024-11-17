60c18b4f5fb08a3b0d35507be57ea154d5a3b3a6
정회민 <76022353+JungHoiMin@users.noreply.github.com>
2024-11-17 21:26:47 +0900

    Delete src directory

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { Quasar } from 'quasar'
-import quasarUserOptions from './quasar-user-options'
-import store from './store'
-import router from './router'
-
-createApp(App).use(router).use(store).use(Quasar, quasarUserOptions).mount('#app')
